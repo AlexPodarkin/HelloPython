8b2f6c1479c88e077ea871cd674a47c7de11c009
Aleksandr <alexchembarov@gmail.com>
2022-12-06 13:34:43 +0400

    committed

--- Seminar4/home_work.py
+++ Seminar4/home_work.py
@@ -13,6 +13,7 @@ def Task1():
     print('число пи с заданной точностью = ', res)
     # print(f'число пи с заданной точностью = {round(pi, count)}') # используя round
 
+
 def Task2():
     divider = 2
     n = 15
@@ -33,18 +34,19 @@ def Task3():
     for i in init_list:
         if i not in result_list:
             result_list.append(i)
-    print('неповторяющиеся элементы:',result_list)
+    print('неповторяющиеся элементы:', result_list)
 
 
 def Task4():
     k = 3
     random_list = []
+    # формируем случайным образом список коэффициентов (значения от 0 до 100)
     for i in range(k + 1):
         random_list.append(random.randint(0, 100))
     result = ""
-    if len(random_list) < 1:
+    if len(random_list) < 1:  # если длина меньше 1 то x = 0
         result = "x = 0"
-    else:
+    else:                   # формируем многочлен
         for i in range(len(random_list)):
             if i != len(random_list) - 1 and random_list[i] != 0 and i != len(random_list) - 2:
                 result += f"{random_list[i]}x^{len(random_list) - i - 1}"
@@ -58,13 +60,15 @@ def Task4():
                 result += f"{random_list[i]} = 0"
             elif i == len(random_list) - 1 and random_list[i] == 0:
                 result += " = 0"
-
-    with open("file.txt", 'w') as data:
+    print('многочлен степени k:', result)
+    with open("file.txt", 'w') as data:  # записываем результат в файл
         data.write(result)
+
 
 Task1()
 print('===============X=================')
 Task2()
 print('===============X=================')
 Task3()
+print('===============X=================')
 Task4()
