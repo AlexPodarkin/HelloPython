ec57288f79fa6c206f94b1fa0f349734c9437639
Aleksandr <alexchembarov@gmail.com>
2022-12-10 23:14:20 +0400

    Lection4

--- HelloPython/Lection203.py
+++
@@ -1,9 +0,0 @@
-def nameFunction (*params):
-    res: str = ""           # переменная двоеточие тип данных, если хотим int пишем res: int = 0 / или просто res = 0 
-    for item in params:
-        res += item         # здесь мы ожидаем получение строки !
-    return res
-
-print(nameFunction('a', 'a', 'a', ))
-print(nameFunction('a', '1', '2', ))
-# print(nameFunction(1, 2 )) # TypeError: ...
---
+++ Lection4/model_view_controller/main.py
@@ -0,0 +1,5 @@
+# точка запуска паттерна model_view_controller
+
+import controller as c
+
+c.button_click()
